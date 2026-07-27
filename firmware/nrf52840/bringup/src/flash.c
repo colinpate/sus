ed41c1c2fd700c0479c900703827d237267630ef
Colin Pate <colin@Colins-MacBook-Air.local>
2026-07-27 12:33:38 -0700

    Fixed flash logic

--- firmware/nrf52840/bringup/src/flash.c
+++ firmware/nrf52840/bringup/src/flash.c
@@ -20,14 +20,11 @@
 #define LOG_READ_INVALID 1
 #define LOG_READ_SERIAL_ERR 2
 #define LOG_READ_SERIAL_DONE 3
+#define LOG_READ_EMPTY 4
 
 uint32_t flash_write_addr;
 uint32_t flash_read_addr;
 uint32_t flash_log_id;
-// flash is full when next write address = read address
-// flash is empty when write address = read address
-bool flash_empty = true; // used to block reading
-bool flash_full = false; // used to block writing
 
 struct flash_chunk {
     uint32_t magic;
@@ -46,6 +43,19 @@ uint32_t flash_next_sector_addr(uint32_t addr){
     }
 }
 
+/*
+ * Keep one sector unused so read_addr == write_addr is unambiguously empty.
+ * write_addr points to the next free sector and read_addr points to the oldest
+ * unread sector.
+ */
+bool flash_is_empty(void){
+    return flash_write_addr == flash_read_addr;
+}
+
+bool flash_is_full(void){
+    return flash_next_sector_addr(flash_write_addr) == flash_read_addr;
+}
+
 void update_log_crc(uint32_t* crc, struct flash_chunk* chunk){
 
 }
@@ -55,9 +65,8 @@ void flash_read_sector(uint32_t addr, struct flash_chunk* chunk){
 }
 
 void flash_erase_sectors(uint32_t start_addr, uint32_t end_addr){
-    // erase all sectors from start_addr to end_addr
+    // erase the half-open range [start_addr, end_addr)
     // wrap around if start > end
-    // 
 }
 
 uint8_t check_chunk(struct flash_chunk* chunk){
@@ -66,16 +75,16 @@ uint8_t check_chunk(struct flash_chunk* chunk){
     return 0;
 }
 
-void flash_scan(){
-    uint32_t lowest_seq = -1;
-    uint32_t lowest_id = -1;
+void flash_scan(void){
+    uint32_t lowest_seq = UINT32_MAX;
+    uint32_t lowest_id = UINT32_MAX;
     uint32_t highest_seq = 0;
     uint32_t highest_id = 0;
     struct flash_chunk curr_chunk;
 
     // Presume empty
     flash_read_addr = FLASH_START_ADDR;
-    flash_write_addr = flash_next_sector_addr(flash_read_addr);
+    flash_write_addr = flash_read_addr;
     bool valid_found = false;
     flash_log_id = 0;
 
@@ -121,58 +130,48 @@ void flash_scan(){
             }
         }
     }
-    flash_log_id = highest_id + 1;
-    flash_empty = (!valid_found);
-    flash_full = (flash_next_sector_addr(flash_write_addr) == flash_read_addr);
+    flash_log_id = valid_found ? highest_id + 1 : 0;
 }
 
-uint8_t flash_read_log(){
+uint8_t flash_read_log(void){
     struct flash_chunk chunk;
     uint32_t addr = flash_read_addr;
     uint32_t log_crc = 0;
     uint32_t read_log_id = 0;
     bool first_read = true;
-    while (true){
+
+    if (flash_is_empty()){
+        return LOG_READ_EMPTY;
+    }
+
+    while (addr != flash_write_addr){
         flash_read_sector(addr, &chunk);
         uint8_t check_result = check_chunk(&chunk);
 
-        if (first_read){
-            if (check_result == 0){
-                read_log_id = chunk.log_id;
-                serial_send_log_start(read_log_id);
-            } else {
-                // No valid log to read here
-                return LOG_READ_INVALID;
-            }
+        if (check_result != 0){
+            return LOG_READ_INVALID;
         }
 
-        // Terminate the log sending if:
-        // Latest read sector is valid and from a new log, or
-        // Latest read sector is empty, or
-        // Flash is empty
-        if (
-            ((chunk.log_id != read_log_id) && (check_result == 0)) || 
-            (check_result == SECTOR_ERASED) ||
-            (addr == flash_write_addr)
-        ){
+        if (first_read){
+            read_log_id = chunk.log_id;
+            serial_send_log_start(read_log_id);
+            first_read = false;
+        } else if (chunk.log_id != read_log_id){
+            // addr still points to the first sector of the next log
             break;
-        } else {
-            serial_send_chunk(&chunk);
         }
+
+        serial_send_chunk(&chunk);
         update_log_crc(&log_crc, &chunk);
-        first_read = false;
         addr = flash_next_sector_addr(addr);
     }
+
     uint8_t ret = serial_send_log_end(read_log_id, log_crc);
     if (ret == SERIAL_LOG_ACK){
         // receiver acknowledged
         // Erase previous log sectors and set read addr to start of next log
         flash_erase_sectors(flash_read_addr, addr);
         flash_read_addr = addr;
-        flash_full = false;
-        if (addr == flash_write_addr) {
-            flash_empty = true;
-        }
     } else if (ret == SERIAL_LOG_ERR) {
         // resend same log
         return LOG_READ_SERIAL_ERR;
@@ -182,21 +181,28 @@ uint8_t flash_read_log(){
     return 0;
 }
 
-void read_flash(){
+void read_flash(void){
     uint8_t num_retries = 0;
-    if (!flash_empty){
+    if (!flash_is_empty()){
         while (true){
             uint8_t ret = flash_read_log();
             if (ret == LOG_READ_SERIAL_ERR){
-                if (num_retries == SERIAL_MAX_RETRIES){
+                if (num_retries >= SERIAL_MAX_RETRIES){
                     break;
                 } else {
                     num_retries += 1;
                 }
-            } else if (ret == LOG_READ_SERIAL_DONE){
+            } else if (
+                (ret == LOG_READ_SERIAL_DONE) ||
+                (ret == LOG_READ_INVALID) ||
+                (ret == LOG_READ_EMPTY)
+            ){
                 break;
-            } else if (flash_empty){
+            } else if (flash_is_empty()){
                 break;
+            } else {
+                // Give each log its own retry budget.
+                num_retries = 0;
             }
         }
     }
--- firmware/nrf52840/bringup/src/flash.h
+++ firmware/nrf52840/bringup/src/flash.h
@@ -1,1 +1,13 @@
+#ifndef SUS_FLASH_H_
+#define SUS_FLASH_H_
+
+#include <stdbool.h>
 #include <stdint.h>
+
+bool flash_is_empty(void);
+bool flash_is_full(void);
+void flash_scan(void);
+uint8_t flash_read_log(void);
+void read_flash(void);
+
+#endif /* SUS_FLASH_H_ */
